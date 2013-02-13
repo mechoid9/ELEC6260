7c6e6b31c72bc882fcd73c08596c8e23a83efb51
mechoid9 <mechoid9@hotmail.com>
2013-02-12 19:42:29 -0600

    Updated PS4_1.s after discussion with other students

--- Assignment06/PS4_1.s
+++ Assignment06/PS4_1.s
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;   Brian Arnberg                       ;;
-;;   Problem Set #4 - Problem 2          ;;
+;;   Problem Set #4 - Problem 1          ;;
 ;;    IIR direct form type II filter     ;;
 ;;      TMS 32C5x DSP                    ;;
 ;; y(n) = b3*v3 + b2*v2 + b1*v1 + b0*v0  ;;
@@ -17,8 +17,9 @@
 	MAC A2, v2	; +a2*v2
 	MAC A1, v1	; +a1*v1 
 	APAC		; A = A+P
-	ADD #X0		; A = A + X0
+	ADD X0		; A = A + X0
 	SACH v0		; save LSB to V0
+	ZAP		; clear A and P
 	MAC B3, v3	; b3*v3
 	MACD B2, v2	; b2*v2, move v2 to v3
 	MACD B1, v1	; b1*v1, move v1 to v2
@@ -27,4 +28,15 @@
 	SACH Yn 	; save LSB
 	OUT Yn,PA1 	; write y(n) to PA1
 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                       ;;
+;;   Program Memory   |   Data Memory    ;;
+;;____________________|__________________;;
+;;    A3 = -a3        |    X0            ;;
+;;    A2 = -a2        |    V0   (low)    ;;
+;;    A1 = -a1        |    V1            ;;
+;;    B0 =  b0        |    V2            ;;
+;;    B1 =  b1        |    V3   (high)   ;;
+;;    B2 =  b2        |                  ;;
+;;    B3 =  b3        |                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
