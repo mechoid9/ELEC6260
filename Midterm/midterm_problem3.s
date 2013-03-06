0350074b384b33944aa9363d33d2b408e125e148
mechoid9 <mechoid9@hotmail.com>
2013-03-05 20:26:40 -0600

    re-formatted programs, added comments

--- Midterm/midterm_problem3.s
+++ Midterm/midterm_problem3.s
@@ -13,21 +13,21 @@ N 	SPACE 4*300	;300 32-bit words
 I	SPACE 4		;1 32-bit word
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	AREA for_loop,CODE,READONLY
-	LDR r8, =M
-	LDR r9, =N
-	LDR r7, =I
-	MOV r2, #0
+	LDR r8, =M		;; initial address for M
+	LDR r9, =N		;; initial address for N
+	LDR r7, =I		;; address for I 
+	MOV r2, #0		;; initialize I
 loop
-	LDR r4, [r8, #4]!		;; M
-	LDR r5, [r9, #4]!		;; N
-	MOV r3, #0x174			;; 23.25 = 0x174 in Q28.4
-	MUL r5, r5, r3			;; N[I]*23.5
-	LSR r5, #4			;; shift right 4 bits
-	AND r4, r5			;; calculate result
-	STR r4, [r8]			;; store to M[I+1]
-	ADD r2, r2, #1			;; I = I + 1
-	CMP r2, 300			;; if (I < 300)
-	BLT loop			;; branch to ``loop''
-	STR r2, [r7]			;; store I
-h	B h				;; end
+	LDR r4, [r8], #4	;; M: load r4 from r8, then index r8
+	LDR r5, [r9], #4	;; N: load r5 from r9, then index r9
+	MOV r3, #0x174		;; 23.25 = 0x174 in Q28.4
+	MUL r5, r5, r3		;; N[I]*23.5
+	LSR r5, #4		;; shift right 4 bits
+	AND r4, r5		;; calculate result
+	STR r4, [r8]		;; store to M[I+1]
+	ADD r2, r2, #1		;; I = I + 1
+	CMP r2, 300		;; if (I < 300)
+	BLT loop		;; branch to ``loop''
+	STR r2, [r7]		;; store I
+h	B h			;; end
 	END
--- Midterm/midterm_problem4.s
+++ Midterm/midterm_problem4.s
@@ -13,15 +13,16 @@ ARRAY	EQU 0x40000000
 	AREA CONVERT,CODE
 	EXPORT convert
 convert
-	ldr r0, =STATUS
-	ldr r1, =DATA
-	ldr r2, =ARRAY
-	MOV r5, #ARRAY
+	LDR r0, =STATUS		;; address of STATUS byte
+	LDR r1, =DATA		;; address of DATA byte
+	LDR r2, =ARRAY		;; initial address of ARRAY
+	LDR r5, =ARRAY
 	ADD r5, r5, #200	;; calculate end of ARRAY
-again	LDRB r3, [r2, #1]!	;;load byte value from ARRAY, increment
-	STRB r3, [r1]		;;store byte to DATA for conversion
-loop	LDRB r4, [r0]		;;load STATUS into r4
-	TST r4, #0x80		;;test leftmost bit of STATUS
+
+again	LDRB r3, [r2, #1]!	;; load byte value from ARRAY, increment
+	STRB r3, [r1]		;; store byte to DATA for conversion
+loop	LDRB r4, [r0]		;; load STATUS into r4
+	TST r4, #0x80		;; test leftmost bit of STATUS
 	BEQ loop		;; if leftmost bit of STATUS is 1, loop
 	CMP r2, r5		;; if (r2 < r5) ; (at the end of the array
 	BLT again		;; loop back to ``again''
